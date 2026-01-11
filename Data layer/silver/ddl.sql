a3efa63d59f870290e7b474906b6bf297b15dcf6
ViictorHugoo <victorhugorodriguesguimaraes@gmail.com>
2026-01-11 17:25:08 -0300

    adiciona ddl e modelagem

---
+++ Data layer/silver/ddl.sql
@@ -0,0 +1,20 @@
+CREATE SCHEMA IF NOT EXISTS silver.cat_db;
+
+DROP TABLE IF EXISTS ACIDENTE;
+
+CREATE TABLE ACIDENTE (
+    agente_causador_acidente TEXT,
+    data_acidente_referencia DATE,
+    cbo_codigo_descricao TEXT,
+    cid_10 TEXT,
+    cnae_empregador TEXT,
+    municipio_empregador TEXT,
+    natureza_lesao TEXT,
+    parte_corpo_atingida TEXT,
+    sexo TEXT,
+    tipo_acidente TEXT,
+    uf_municipio_acidente TEXT,
+    uf_municipio_empregador TEXT,
+    data_nascimento DATE,
+    data_emissao_cat DATE
+);
