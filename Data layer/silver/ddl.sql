c3c8a6aefe1796bc9ed081be9c9eca373825f424
Vinícius Mendes <96599913+yabamiah@users.noreply.github.com>
2026-01-14 21:18:28 -0300

    ref: atualizando ddl

--- Data layer/silver/ddl.sql
+++ Data layer/silver/ddl.sql
@@ -4,10 +4,11 @@ DROP TABLE IF EXISTS ACIDENTE;
 
 CREATE TABLE ACIDENTE (
     agente_causador_acidente TEXT,
-    data_acidente_referencia DATE,
+    data_acidente_referencia TEXT,
     cbo_codigo_descricao TEXT,
     cid_10 TEXT,
-    cnae_empregador TEXT,
+	cnae_empregador BIGINT,
+    cnae_empregador_descricao TEXT,
     municipio_empregador TEXT,
     natureza_lesao TEXT,
     parte_corpo_atingida TEXT,
