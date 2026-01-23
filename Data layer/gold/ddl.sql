1f89070746a22035cd10350bb012cd86a3a5c918
yabamiah <viniciusmendes1019@gmail.com>
2026-01-22 22:42:47 -0300

    ref: Atualizando nome de chaves srk para três letras cada palavra

--- Data layer/gold/ddl.sql
+++ Data layer/gold/ddl.sql
@@ -2,8 +2,8 @@ CREATE SCHEMA IF NOT EXISTS gold;
 
 DROP TABLE IF EXISTS gold.dim_tempo CASCADE;
 CREATE TABLE gold.dim_tempo (
-    srk_temp BIGSERIAL PRIMARY KEY,
-    chv_tempo_org DATE NOT NULL UNIQUE,
+    srk_tmp BIGSERIAL PRIMARY KEY,
+    chv_tmp_org DATE NOT NULL UNIQUE,
     dia INTEGER,
     mes INTEGER,
     nome_mes VARCHAR(50),
@@ -23,65 +23,65 @@ CREATE TABLE gold.dim_cbo (
 
 DROP TABLE IF EXISTS gold.dim_municipio CASCADE;
 CREATE TABLE gold.dim_municipio (
-    srk_munic BIGSERIAL PRIMARY KEY,
-    chv_municipio_org TEXT NOT NULL UNIQUE,
+    srk_mnc BIGSERIAL PRIMARY KEY,
+    chv_mnc_org TEXT NOT NULL UNIQUE,
     nome TEXT,
     uf TEXT
 );
 
 DROP TABLE IF EXISTS gold.dim_cnae CASCADE;
 CREATE TABLE gold.dim_cnae (
-    srk_cnae BIGSERIAL PRIMARY KEY,
-    chv_cnae_org BIGINT NOT NULL UNIQUE,
+    srk_cne BIGSERIAL PRIMARY KEY,
+    chv_cne_org BIGINT NOT NULL UNIQUE,
     codigo BIGINT,
     descricao TEXT
 );
 
 DROP TABLE IF EXISTS gold.dim_tipo_acidente CASCADE;
 CREATE TABLE gold.dim_tipo_acidente (
-    srk_tp_acdt BIGSERIAL PRIMARY KEY,
-    chv_tipo_acidente_org TEXT NOT NULL UNIQUE,
+    srk_tpo_act BIGSERIAL PRIMARY KEY,
+    chv_tpo_act_org TEXT NOT NULL UNIQUE,
     descricao TEXT
 );
 
 DROP TABLE IF EXISTS gold.dim_lesao CASCADE;
 CREATE TABLE gold.dim_lesao (
-    srk_lesao BIGSERIAL PRIMARY KEY,
-    chv_lesao_org TEXT NOT NULL UNIQUE,
+    srk_lso BIGSERIAL PRIMARY KEY,
+    chv_lso_org TEXT NOT NULL UNIQUE,
     natureza_lesao TEXT,
     parte_corpo_atingida TEXT
 );
 
 DROP TABLE IF EXISTS gold.dim_agente_causador CASCADE;
 CREATE TABLE gold.dim_agente_causador (
-    srk_agnt_csdr BIGSERIAL PRIMARY KEY,
-    chv_agente_causador_org TEXT NOT NULL UNIQUE,
+    srk_agt_cdr BIGSERIAL PRIMARY KEY,
+    chv_agt_cdr_org TEXT NOT NULL UNIQUE,
     descricao TEXT
 );
 
 DROP TABLE IF EXISTS gold.dim_cid10 CASCADE;
 CREATE TABLE gold.dim_cid10 (
-    srk_cid10 BIGSERIAL PRIMARY KEY,
-    chv_cid10_org TEXT NOT NULL UNIQUE,
+    srk_cid BIGSERIAL PRIMARY KEY,
+    chv_cid_org TEXT NOT NULL UNIQUE,
     codigo TEXT,
     descricao TEXT
 );
 
 DROP TABLE IF EXISTS gold.dim_trabalhador CASCADE;
 CREATE TABLE gold.dim_trabalhador (
-    srk_trab BIGSERIAL PRIMARY KEY,
-    chv_trabalhador_org TEXT NOT NULL UNIQUE,
+    srk_trb BIGSERIAL PRIMARY KEY,
+    chv_trb_org TEXT NOT NULL UNIQUE,
     sexo TEXT,
     srk_cbo TEXT,
-    srk_temp_nasc DATE
+    srk_tmp_nsc DATE
 );
 
 DROP TABLE IF EXISTS gold.dim_empregador CASCADE;
 CREATE TABLE gold.dim_empregador (
-    srk_empreg BIGSERIAL PRIMARY KEY,
-    chv_empregador_org TEXT NOT NULL UNIQUE,
-    srk_cnae BIGINT,
-    srk_munic TEXT
+    srk_emp BIGSERIAL PRIMARY KEY,
+    chv_emp_org TEXT NOT NULL UNIQUE,
+    srk_cne BIGINT,
+    srk_mnc TEXT
 );
 
 DROP TABLE IF EXISTS gold.fato_acidente_trabalho CASCADE;
@@ -89,22 +89,22 @@ CREATE TABLE gold.fato_acidente_trabalho (
     srk_fato BIGSERIAL PRIMARY KEY,
     chv_cat_org TEXT NOT NULL,
     
-    srk_temp_acdt BIGINT REFERENCES gold.dim_tempo(srk_temp),
-    srk_temp_emss BIGINT REFERENCES gold.dim_tempo(srk_temp),
-    srk_temp_nasc BIGINT REFERENCES gold.dim_tempo(srk_temp),
+    srk_tmp_act BIGINT REFERENCES gold.dim_tempo(srk_tmp),
+    srk_tmp_ems BIGINT REFERENCES gold.dim_tempo(srk_tmp),
+    srk_tmp_nsc BIGINT REFERENCES gold.dim_tempo(srk_tmp),
     
-    srk_trab BIGINT REFERENCES gold.dim_trabalhador(srk_trab),
+    srk_trb BIGINT REFERENCES gold.dim_trabalhador(srk_trb),
     srk_cbo BIGINT REFERENCES gold.dim_cbo(srk_cbo),
-    srk_empreg BIGINT REFERENCES gold.dim_empregador(srk_empreg),
-    srk_cnae BIGINT REFERENCES gold.dim_cnae(srk_cnae),
+    srk_emp BIGINT REFERENCES gold.dim_empregador(srk_emp),
+    srk_cne BIGINT REFERENCES gold.dim_cnae(srk_cne),
     
-    srk_munic_acdt BIGINT REFERENCES gold.dim_municipio(srk_munic),
-    srk_munic_empreg BIGINT REFERENCES gold.dim_municipio(srk_munic),
+    srk_mnc_act BIGINT REFERENCES gold.dim_municipio(srk_mnc),
+    srk_mnc_emp BIGINT REFERENCES gold.dim_municipio(srk_mnc),
     
-    srk_tp_acdt BIGINT REFERENCES gold.dim_tipo_acidente(srk_tp_acdt),
-    srk_lesao BIGINT REFERENCES gold.dim_lesao(srk_lesao),
-    srk_agnt_csdr BIGINT REFERENCES gold.dim_agente_causador(srk_agnt_csdr),
-    srk_cid10 BIGINT REFERENCES gold.dim_cid10(srk_cid10),
+    srk_tpo_act BIGINT REFERENCES gold.dim_tipo_acidente(srk_tpo_act),
+    srk_lso BIGINT REFERENCES gold.dim_lesao(srk_lso),
+    srk_agt_cdr BIGINT REFERENCES gold.dim_agente_causador(srk_agt_cdr),
+    srk_cid BIGINT REFERENCES gold.dim_cid10(srk_cid),
     
     idade_trabalhador INTEGER
 );
